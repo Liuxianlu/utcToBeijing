8635731f49ee9e3ac2007434386bd355a801e29f
liuxianlu <70340008@qq.com>
2020-05-08 21:56:28 +0800

    1.0

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="content"></view>
+</template>
+
+<script>
+  import utils from '@/utils/utils.js';
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    onLoad() {
+      console.log(utils.utcToBeijing('2020-05-08T04:25:44.000Z')); // 2020/05/08 12:25
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+  
+</style>
